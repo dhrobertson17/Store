1112796519f26f7d7100061a551aa6a7a4c0ac76
Daniel Robertson <dhrobertson17@gmail.com>
2018-07-09 11:31:51 -0400

    added parameter inputs for cart controller

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,5 +1,9 @@
 class CartController < ApplicationController
   def add_to_cart
+  	line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity])
+  	line_item.update(line_item_total: (line_item.quantity * line_item.product.price))
+
+  	redirect_back(fallback_location: root_path)
   end
 
   def view_order

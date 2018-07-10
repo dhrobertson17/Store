7bbc720f4ae1a6e082bcd20741c0e11157567551
Daniel Robertson <dhrobertson17@gmail.com>
2018-07-10 09:43:59 -0400

    Added cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,22 @@
 class ApplicationController < ActionController::Base
 
-	before_action :categories, :brands
+  before_action :categories, :brands
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-	def categories
-		@categories = Category.order(:name)
-	end
+  def categories
+  	@categories = Category.order(:name)
+  end
 
-	def brands
-		@brands = Product.pluck(:brand).sort.uniq
-	end
+  def brands
+  	@brands = Product.pluck(:brand).sort.uniq
+  end
 
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:role])
+  end
 end
